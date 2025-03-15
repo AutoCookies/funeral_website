eb04a523358f381b11b006a83d516182db0f74d1
AutoCookies <fhhdhysd@gmail.com>
2025-03-15 12:46:30 +0700

    Update

--- week2_2280602627/obj/Release/net8.0/week2_2280602627.AssemblyInfo.cs
+++ week2_2280602627/obj/Release/net8.0/week2_2280602627.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("week2_2280602627")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e48260dda9ba8fbeb578fb7c963a54e90001ff96")]
 [assembly: System.Reflection.AssemblyProductAttribute("week2_2280602627")]
