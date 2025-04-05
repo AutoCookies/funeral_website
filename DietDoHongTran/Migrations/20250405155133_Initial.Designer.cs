e1ab3cf274e3d0c6a363349c6c030a3238dac465
Cookiescooker <fhhdhysd@gmail.com>
2025-04-05 23:23:31 +0700

    Update datatable cho category trong admin

--- DietDoHongTran/Migrations/20250405155133_Initial.Designer.cs
+++ DietDoHongTran/Migrations/20250405155133_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DietDoHongTran.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250405063309_Initial")]
+    [Migration("20250405155133_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
